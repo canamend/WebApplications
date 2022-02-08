2c03229c4a17320609cf9df568ae6ee7a9c40d2b
Jonathan De La Cruz Huerta <thefallensclasic@gmail.com>
2022-02-08 09:45:17 -0600

    Actividad 5 sin formato

---
+++ actividad4/index.php
@@ -0,0 +1,76 @@
+<?php
+contarVotos();
+function contarVotos(){
+    $votos=[];
+    $fp=fopen("count.dat","r");
+    $contenido_fichero = fread($fp, filesize("count.dat"));
+    while (!feof($fp)){
+        $linea = fgets($fp);
+        echo $linea;
+    }
+    fclose($fp);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 4</title>
+    <link rel="stylesheet" href="assets/normalize.css">
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <main class="middle">
+        <form action="votar.php">
+            <section>
+                <label for="">Nombre: </label>
+                <input type="text" name="nombre">
+            </section>
+            <div class="partidos">
+                <button type="submit" name="Nueva_Alianza" value="Nueva_Alianza"><img src="assets/seleccion.png" class="select"><img src="assets/partido1.png" alt="partido1"></button>
+                <button type="submit" name="Movimiento_Ciudadano" value="Movimiento_Ciudadano"><img src="assets/seleccion.png" class="select"><img src="assets/partido2.png" alt="partido2"></button>
+                <button type="submit" name="PRI" value="PRI"><img src="assets/seleccion.png" class="select"><img src="assets/partido3.png" alt="partido3"></button>
+                <button type="submit" name="PRD" value="PRD"><img src="assets/seleccion.png" class="select"><img src="assets/partido4.png" alt="partido4"></button>
+                <button type="submit" name="PAN" value="PAN"><img src="assets/seleccion.png" class="select"><img src="assets/partido5.png" alt="partido5"></button>
+                <button type="submit" name="Verde" value="Verde"><img src="assets/seleccion.png" class="select"><img src="assets/partido6.png" alt="partido6"></button>
+                <button type="submit" name="PES" value="PES"><img src="assets/seleccion.png" class="select"><img src="assets/partido7.png" alt="partido7"></button>
+                <button type="submit" name="Morena" value="Morena"><img src="assets/seleccion.png" class="select"><img src="assets/partido8.png" alt="partido8"></button>
+                <button type="submit" name="NULL" value="NULL"><img src="assets/seleccion.png" class="select"><img src="assets/calcelar.png" alt="anular"></button>
+            </div>
+        </form>
+    </main>
+    <footer>
+        <div>
+            <ul>
+                <li>BUAP</li>
+                <li>F.C.C</li>
+                <li>Aplicaciones WEB</li>
+                <li>2022</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>Nueva_Alianza</li>
+                <li>Movimiento_Ciudadano</li>
+                <li>PRI</li>
+                <li>PRD</li>
+                <li>PAN</li>
+                <li>Verde</li>
+                <li>PES</li>
+                <li>Morena</li>
+                <li>NULL</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>Canalizo Mendoza Isaac A.</li>
+                <li>De La Cruz Huerta Jonathan</li>
+                <li>Angel Hernandez Ramos</li>
+                <li>Pérez Hernandez Jorge S.</li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
